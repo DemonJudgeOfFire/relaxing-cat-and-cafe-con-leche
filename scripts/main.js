4c35dc23cff1e926d021287c995ccca07efa6862
Antonio <jhonbairon.jb@gmail.com>
2021-07-11 17:20:56 -0400

    Added a new image to the images folder and edited the main.js file with a .onclick method

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-let my_heading = document.querySelector('h1');
-my_heading.textContent = 'Hello, World!';
+let my_image = document.querySelector("img");
+
+my_image.onclick = function() {
+    let my_src = my_image.getAttribute("src");
+    if(my_src === "images/cat_coffee.jpg") {
+        my_image.setAttribute("src", "images/pexels-marko-blazevic-774731.jpg");
+    } else {
+        my_image.setAttribute("src", "images/cat_coffee.jpg")
+    }
+}
